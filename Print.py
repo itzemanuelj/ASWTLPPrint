673f9a0709f29c7001e358cc34d0c116f20d2969
Emanuel Joness <itzemanuelj@gmail.com>
2022-08-10 01:06:31 -0400

    initial commit

---
+++ Print.py
@@ -0,0 +1,17 @@
+# What keyword tells Python to display something on the screen?
+import string
+
+
+print
+# Complete the line of code to tell Python to display the name of the oval object laid by a hen. (1 word, lower-case)
+# print________
+print ("egg")
+# Tell Python to display the letter "e".
+print ("e")
+# Tell Python to display the name of our planet. (1 word, lower-case)
+print("earth")
+# Tell Python to display the name of the star that makes life possible on earth. (1 word, lower-case)
+print("sun")
+# In a print statement, the characters surrounded by quotation marks are a text _______.
+# Press ctrl-Enter or click button to check answer
+string
